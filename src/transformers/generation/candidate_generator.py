c967bbede6e95eb2474e8bd32cd5849e12e18a46
N <matrix.yao@intel.com>
2024-11-19 09:53:04 -0500

    remove warnings import
    Signed-off-by: YAO Matrix <matrix.yao@intel.com>

--- src/transformers/generation/candidate_generator.py
+++ src/transformers/generation/candidate_generator.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import warnings
-
 import copy
 from typing import TYPE_CHECKING, Any, Dict, Optional, Tuple
 

587b55f8d91c6bb24bc0d80bbf1ce4dbd9a3c4c8
N <matrix.yao@intel.com>
2024-12-02 10:38:38 -0500

    skip Gemma2, it declars support static cache, but it's hybrid cache actually
    Signed-off-by: N <matrix.yao@intel.com>

--- tests/generation/test_utils.py
+++ tests/generation/test_utils.py
@@ -2088,7 +2088,7 @@ class GenerationTesterMixin:
             without_all_logits = model.generate(**inputs_dict, **generation_kwargs)
             self.assertEqual(with_all_logits.tolist(), without_all_logits.tolist())
 
-    @parameterized.expand([(None, True), ("static", False)])
+    @parameterized.expand([("static", False), (None, True)])
     @pytest.mark.generate
     def test_assisted_decoding_with_num_logits_to_keep(self, cache_implementation, return_legacy_cache):
         for model_class in self.all_generative_model_classes:

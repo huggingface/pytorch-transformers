ea8324dc0aa103a337745da12d41fe5bea518f9a
bakrianoo <bakrianoo@gmail.com>
2025-01-03 19:55:37 +0200

    update __init__ loading

--- src/transformers/utils/dummy_pt_objects.py
+++ src/transformers/utils/dummy_pt_objects.py
@@ -6480,6 +6480,11 @@ class ModernBertPreTrainedModel(metaclass=DummyObject):
     def __init__(self, *args, **kwargs):
         requires_backends(self, ["torch"])
 
+class ModernBertForQuestionAnswering(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
 
 class MoshiForCausalLM(metaclass=DummyObject):
     _backends = ["torch"]

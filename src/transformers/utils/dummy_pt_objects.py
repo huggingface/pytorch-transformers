48e46d896e5e70d5d073fcea4e8750b2a56dd078
Abu Bakr Soliman <bakrianoo@gmail.com>
2025-01-08 09:39:57 +0000

    apply make fix-copies

--- src/transformers/utils/dummy_pt_objects.py
+++ src/transformers/utils/dummy_pt_objects.py
@@ -6480,11 +6480,13 @@ class ModernBertPreTrainedModel(metaclass=DummyObject):
     def __init__(self, *args, **kwargs):
         requires_backends(self, ["torch"])
 
+
 class ModernBertForQuestionAnswering(metaclass=DummyObject):
     _backends = ["torch"]
 
     def __init__(self, *args, **kwargs):
         requires_backends(self, ["torch"])
+
 
 class MoshiForCausalLM(metaclass=DummyObject):
     _backends = ["torch"]

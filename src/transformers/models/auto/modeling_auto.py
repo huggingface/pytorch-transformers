724134e36c338a19af2c73274aeb1d4fee4cf2d0
Abu Bakr Soliman <bakrianoo@gmail.com>
2025-01-08 12:45:01 +0000

    append ModernBertForQuestionAnswering to the pipeline supported models

--- src/transformers/models/auto/modeling_auto.py
+++ src/transformers/models/auto/modeling_auto.py
@@ -1112,6 +1112,7 @@ MODEL_FOR_QUESTION_ANSWERING_MAPPING_NAMES = OrderedDict(
         ("xlnet", "XLNetForQuestionAnsweringSimple"),
         ("xmod", "XmodForQuestionAnswering"),
         ("yoso", "YosoForQuestionAnswering"),
+        ("modernbert", "ModernBertForQuestionAnswering"),
     ]
 )
 

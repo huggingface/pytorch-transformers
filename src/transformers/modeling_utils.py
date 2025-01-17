e7ceedc8f8d548f04908f7152a7a7aec296e84f6
Cyril Vallez <cyril.vallez@gmail.com>
2025-01-17 15:27:34 +0000

    Update modeling_utils.py

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -5189,6 +5189,10 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin, GenerationMixin, PushToHubMix
             self._last_compile_config = compile_config
             self._compiled_call = torch.compile(self.__call__, **compile_config.to_dict())
         return self._compiled_call
+    
+    @classmethod
+    def is_backend_compatible(cls):
+        return cls._supports_attention_backend
 
 
 PreTrainedModel.push_to_hub = copy_func(PreTrainedModel.push_to_hub)
